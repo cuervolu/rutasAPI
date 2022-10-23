9ec6a68b8bcb151690f190109bfa73a84849918b
cuervolu <cuervolu@protonmail.com>
2022-10-23 19:48:49 -0300

    fix: correción de elementos olvidados

--- rutas/serializers.py
+++ rutas/serializers.py
@@ -1,33 +1,30 @@
 from rest_framework import serializers
 from .models import *
 
-
 class OrigenSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Origen
         fields = '__all__'
-
-
 class DestinoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Destino
         fields = '__all__'
-
-
-class PasajeroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pasajero
-        fields = '__all__'
-
 class WayPointSerializer(serializers.ModelSerializer):
     class Meta:
         model = Waypoint
         fields = '__all__'
+class PasajeroSerializer(serializers.ModelSerializer):
+    waypoint = WayPointSerializer(many=True)
+    class Meta:
+        model = Pasajero
+        fields = '__all__'
 
 class RutasSerializer(serializers.ModelSerializer):
     origen = OrigenSerializer()
+    destino = DestinoSerializer()
+    pasajero = PasajeroSerializer(many=True)
     class Meta:
         model = Rutas
         fields = ('__all__')
