1a81e6c4618308e88a7eaa8a6c25277528071c46
cuervolu <cuervolu@protonmail.com>
2022-10-23 18:07:38 -0300

    feat: Creación de Modelo WayPoints

--- rutas/models.py
+++ rutas/models.py
@@ -2,23 +2,37 @@ from django.db import models
 
 # Create your models here.
 
+
 class Origen(models.Model):
     latitud = models.FloatField()
     longitud = models.FloatField()
+
+
 class Destino(models.Model):
     latitud = models.FloatField()
     longitud = models.FloatField()
+
+
+class Waypoint(models.Model):
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+
+
 class Pasajero(models.Model):
     uid = models.CharField(max_length=200)
+    waypoint = models.ManyToManyField(Waypoint, blank=True)
+
+    def __str__(self):
+        return self.uid
+
 
 class Rutas(models.Model):
     direccion = models.CharField(max_length=500)
     chofer = models.CharField(max_length=150)
     fecha = models.DateTimeField(auto_now_add=True)
-    origen = models.OneToOneField(Origen,on_delete=models.CASCADE)
-    destino = models.OneToOneField(Destino,on_delete=models.CASCADE)
-    pasajero = models.OneToOneField(Pasajero,on_delete=models.CASCADE)
-    
+    origen = models.OneToOneField(Origen, on_delete=models.CASCADE)
+    destino = models.OneToOneField(Destino, on_delete=models.CASCADE)
+    pasajero = models.ManyToManyField(Pasajero)
 
     def __str__(self):
         return self.direccion
